8b13573fbc464580a85914b3483bf5c472d4a507
Briana Endanawas <brianamay48@gmail.com>
2022-05-15 17:46:04 -0400

    add sign in page css

--- onestop-daycare/src/components/SignIn.js
+++ onestop-daycare/src/components/SignIn.js
@@ -1,24 +1,30 @@
+import "./SignIn.scss";
+
 function SignIn() {
   return (
-    <div className="SignIn">
-      <div className="Logo">
-        <img
-          src="https://www.adaptivewfs.com/wp-content/uploads/2020/07/logo-placeholder-image.png"
-          alt="Logo Picture"
-        />
-        <div>
-          <p>the right daycare solutions for you</p>
-          <p>extra text and info</p>
+    <div className="Main">
+      <div className="SignIn">
+        <div className="Logo">
+          <img
+            className="LogoImg"
+            src="https://www.adaptivewfs.com/wp-content/uploads/2020/07/logo-placeholder-image.png"
+            alt="Logo Picture"
+          />
+          <div className="Text">
+            <p>the right daycare solutions for you</p>
+          </div>
         </div>
-      </div>
-      <div className="UserInfo">
-        <form>
-          <input type="text" placeholder="email/username"></input>
-          <input type="text" placeholder="password"></input>
-          <button type="button">SIGN IN</button>
-          <p>Don't already have an account?</p>
-          <button type="button">CREATE AN ACCOUNT</button>
-        </form>
+        <div className="UserInfo">
+          <form className="UserInfoForm">
+            <input type="text" placeholder="email/username" id="user"></input>
+            <input type="text" placeholder="password" id="password"></input>
+            <button type="button" id="sign">SIGN IN</button>
+            <div className="NoAccount">
+              <p>Don't already have an account?</p>
+            </div>
+            <button type="button" id="create">CREATE AN ACCOUNT</button>
+          </form>
+        </div>
       </div>
     </div>
   );
