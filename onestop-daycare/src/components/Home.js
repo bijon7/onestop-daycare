14d1c82b9b6a3117f96e9e01949c8bff2d0f1f37
Abdullah Ahmad Zabir <zabir.aa@gmail.com>
2022-05-17 23:19:54 -0400

    Delete redundant onestop-daycare folder

--- onestop-daycare/src/components/Home.js
+++
@@ -1,9 +0,0 @@
-const Home = () => {
-  return (
-    <div>
-      <h2>This is the Home component</h2>
-    </div>
-  );
-};
-
-export default Home;
--- onestop-daycare/src/pages/GuardianDashboard.js
+++
@@ -1,9 +0,0 @@
-const GuardianDashboard = () => {
-  return (
-    <div>
-      <h2>This is the Guardian Dashboard component</h2>
-    </div>
-  );
-};
-
-export default GuardianDashboard;
