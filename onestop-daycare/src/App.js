82ca01624724d082f7b35c64fc17f31c9a4e2614
Briana Endanawas <brianamay48@gmail.com>
2022-05-17 22:03:06 -0400

    react router, layouts for front end, users schema

--- onestop-daycare/src/App.js
+++ onestop-daycare/src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import { BrowserRouter, Link, Route, Routes, Navigate } from 'react-router-dom';
+import SignIn from './components/SignIn';
+//import Home from './components/Home';
+import About from './components/About';
+import AdminDashboard from './pages/AdminDashboard';
+import GuardianDashboard from './pages/GuardianDashboard';
 
 function App() {
+  const [userType, setUserType] = useState("admin");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <BrowserRouter>
+      <div className="App">
+      <nav>
+        <Link to="/">Home</Link>{' '}
+        <Link to="/about">About</Link>{' '}
+        <Link to="/signin">Sign In</Link>{' '}
+      </nav>
+        {/* <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
         </a>
-      </header>
-    </div>
+        </header> */}
+        <Routes>
+          {userType === "admin" ? <Route path="/" element={<AdminDashboard />} /> : <Route path="/" element={<GuardianDashboard />} />}
+          <Route path="about" element={<About />} />
+          <Route path="signin" element={<SignIn />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ onestop-daycare/src/pages/GuardianDashboard.js
@@ -0,0 +1,9 @@
+const GuardianDashboard = () => {
+  return (
+    <div>
+      <h2>This is the Guardian Dashboard component</h2>
+    </div>
+  );
+};
+
+export default GuardianDashboard;
