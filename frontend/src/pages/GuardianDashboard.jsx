bdfdbfa020a4eb1d1dd6cbd53f5eca8e66b11ac8
vanlgo <goellevan@gmail.com>
2022-05-20 23:13:29 -0400

    css for nav bar, as well as sections for guardian-dashboard completed

--- frontend/src/pages/GuardianDashboard.jsx
+++ frontend/src/pages/GuardianDashboard.jsx
@@ -1,9 +1,23 @@
-const GuardianDashboard = () => {
+import { Paper } from "@mui/material"
+import "./GuardianDashboard.scss";
+
+export default function GuardianDashboard() {
   return (
-    <div>
-      <h2>This is the Guardian Dashboard component</h2>
+    <div className="guardian-dashboard">
+      <div className="guardian-dashboard_sections">
+        <Paper className="guardian-dashboard_mid">
+          <h1>Search Our Daycares</h1>
+        </Paper>
+        <Paper className="guardian-dashboard_mid">
+          <h1>My Children</h1>
+        </Paper>
+      </div>
+      <Paper className="guardian-dashboard_col">
+        <h1>Short List</h1>
+      </Paper>
+      <Paper className="guardian-dashboard_col">
+        <h1>Applications</h1>
+      </Paper>
     </div>
   );
 };
-
-export default GuardianDashboard;
